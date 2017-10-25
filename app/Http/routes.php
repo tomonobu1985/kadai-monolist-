9e002838a282f4f9bc89ebb5dfbf691c2524e1b4
tomonobu <tomonobu.sasaki@bravi.co.jp>
2017-10-25 05:01:50 +0000

    want

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,4 +28,11 @@ Route::group(['middleware' => 'auth'], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create']]);
 });
 
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
+    Route::post('want', 'ItemUserController@want')->name('item_user.want');
+    Route::delete('want', 'ItemUserController@dont_want')->name('item_user.dont_want');
+    Route::resource('users', 'UsersController', ['only' => ['show']]);
+});
+
 
