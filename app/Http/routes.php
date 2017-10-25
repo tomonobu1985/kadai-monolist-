5ed803ef415f443703fac935134dd4ddaf590a96
tomonobu <tomonobu.sasaki@bravi.co.jp>
2017-10-25 04:44:33 +0000

    rakuten api

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,4 +24,8 @@ Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
 Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
 Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('items', 'ItemsController', ['only' => ['create']]);
+});
+
 
